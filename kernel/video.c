63e4037ef4fff6db917301036ad4ff843b295b86
Wonicon <conciselove@outlook.com>
2016-02-19 16:19:04 +0800

    Double buffer and some drawing functions

--- kernel/video.c
+++ kernel/video.c
@@ -1,4 +1,6 @@
 #include "video.h"
+#include "debug.h"
+#include <inc/string.h>
 
 /**
  * 显式模式具体信息
@@ -35,6 +37,7 @@ static ModeInfoBlock mode_info = {};
  * 显存地址
  */
 static uint8_t *vmem = NULL;
+static uint8_t vmem_buffer[800 * 600 * 3];
 
 /**
  * 将 mode info 保存到内核空间
@@ -48,7 +51,32 @@ init_video(void)
      * 此片区域可以保证没有预设内容, 但要小心代码对这里进行的写入
      */
     mode_info = *(ModeInfoBlock *)0xf000;
+    LOG_EXPR(mode_info.pitch);
     vmem = (void *)mode_info.physbase;
+}
+
+uint32_t
+get_scr_w(void)
+{
+    return mode_info.Xres;
+}
+
+uint32_t
+get_scr_h(void)
+{
+    return mode_info.Yres;
+}
+
+void
+init_video_buffer(void)
+{
+    memset(vmem_buffer, 0, sizeof(vmem_buffer));
+}
+
+void
+update_screen(void)
+{
+    memcpy(vmem, vmem_buffer, sizeof(vmem_buffer));
 }
 
 /**
@@ -58,7 +86,7 @@ void
 draw_pixel_bgr(int x, int y, uint8_t b, uint8_t g, uint8_t r)
 {
     if (0 <= x && x < mode_info.Xres && 0 <= y && y < mode_info.Yres) {
-        uint8_t *pixel = vmem + y * mode_info.pitch + x * 3;  // BGR BGR 连续存储
+        uint8_t *pixel = vmem_buffer + y * mode_info.pitch + x * 3;  // BGR BGR 连续存储
         *pixel++ = b;
         *pixel++ = g;
         *pixel++ = r;
@@ -74,3 +102,50 @@ draw_pixel(int x, int y, int color)
     draw_pixel_bgr(x, y, color & 0xff, (color >> 8) & 0xff, (color >> 16) & 0xff);
 }
 
+
+/**
+ * RGB format in bgr[]:
+ *     +-+-+-+-+-+-+
+ *  ...|b|g|r|b|g|r|...
+ *     +-+-+-+-+-+-+
+ *      ^
+ *      bgr[y * w * 3 + x * 3]
+ */
+uint32_t
+get_color(uint8_t bgr[], int x, int y, int w, int h)
+{
+    int index = (y * w + x) * 3;
+    return bgr[index] | (bgr[index + 1] << 8) | (bgr[index + 2] << 16);
+}
+
+/**
+ * Center-aligned:
+ *
+ * +-width-+
+ * +-+-+-+-+
+ * |c|c|c|c|
+ * +-+-+-+-+
+ *      ^
+ *      x
+ */
+void
+draw_column(int x, int y, int width, int length, uint32_t color)
+{
+    x = x - width / 2;
+    for (int off_y = 0; off_y < length; off_y++) {
+        for (int off_x = 0; off_x < width; off_x++) {
+            draw_pixel(x + off_x, y + off_y, color);
+        }
+    }
+}
+
+void
+draw_row(int x, int y, int width, int length, uint32_t color)
+{
+    y = y - width / 2;
+    for (int off_y = 0; off_y < width; off_y++) {
+        for (int off_x = 0; off_x < length; off_x++) {
+            draw_pixel(x + off_x, y + off_y, color);
+        }
+    }
+}
--- kernel/video.h
+++ kernel/video.h
@@ -5,8 +5,22 @@
 
 void init_video(void);
 
+uint32_t get_scr_w(void);
+
+uint32_t get_scr_h(void);
+
 void draw_pixel_bgr(int x, int y, uint8_t b, uint8_t g, uint8_t r);
 
 void draw_pixel(int x, int y, int color);
+
+uint32_t get_color(uint8_t bgr[], int x, int y, int w, int h);
+
+void draw_column(int x, int y, int width, int length, uint32_t color);
+
+void draw_row(int x, int y, int width, int length, uint32_t color);
+
+void init_video_buffer(void);
+
+void update_screen(void);
 
 #endif  // KERNEL_VIDEO_H
